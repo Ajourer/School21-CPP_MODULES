e047ac8a145fd9612fc9d76a5c432027ccb87c15
Regine Alverta <ralverta@oa-m5.msk.21-school.ru>
2022-01-21 15:11:34 +0300

    ex06 is done :)

---
+++ CPP_01/ex06/main.cpp
@@ -0,0 +1,14 @@
+#include "Karen.hpp"
+
+int main(int ac, char **av)
+{
+	Karen(karen);
+	switch (ac) {
+		case 2:
+			karen.complain(av[1]);
+			return (0);
+		default:
+			std::cout << "\033[31mError: wrong parameter\033[0m" << std::endl;
+			return (0);
+	}
+}
