badc9e9693229fd9fac13718050ac9f1776ed5e0
Regine Alverta <ralverta@su-a4.msk.21-school.ru>
2022-01-24 21:09:22 +0300

    ex00 from CPP02 done

---
+++ CPP_02/ex00/Fixed.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include "Fixed.hpp"
+
+const int Fixed::bits = 8;
+
+Fixed::Fixed() : value(0) {
+	std::cout << "Default constructor called" << std::endl;
+}
+
+Fixed::~Fixed() {
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed& fixed) : value(fixed.value) {
+	std::cout << "Copy constructor called" << std::endl;
+}
+
+Fixed &Fixed::operator= (const Fixed& fixed) {
+	std::cout << "Assignation operator called" << std::endl;
+	if (this == &fixed) //проверка на самоприсваивание
+		return (*this);
+	value = fixed.value;
+	return (*this);
+}
+
+int Fixed::getRawBits() const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return (this->value);
+}
+
+void Fixed::setRawBits(const int raw)
+{
+	this->value = raw;
+}
