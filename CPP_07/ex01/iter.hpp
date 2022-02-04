a5b24e74af212e60f57bc629032b5aab845ffb45
Regine Alverta <ralverta@mi-e3.msk.21-school.ru>
2022-02-04 21:05:19 +0300

    ex01 is done

---
+++ CPP_07/ex01/iter.hpp
@@ -0,0 +1,21 @@
+#ifndef ITER_H
+#define ITER_H
+#include <iostream>
+
+template<typename T>
+void cout(T& name) {
+	std::cout << name << std::endl;
+}
+
+template<typename T>
+void multiply(T& name) {
+	std::cout << name * 2 << std::endl;
+}
+
+template<typename T>
+void iter(T* arr, int len, void(*function)(T&)) {
+	for (int i = 0; i < len; i++)
+		function(arr[i]);
+}
+
+#endif
